55b59a0d12547979dd8991f6b33b90e95021a0c5
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2024-02-23 15:09:29 -0700

    added requires auth

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { auth } = require('express-openid-connect');
+const { auth, requiresAuth } = require('express-openid-connect');
 const dotenv = require('dotenv');
 dotenv.config();
 
