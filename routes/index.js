f1ea5e4f327215c770c83547bf5afb6403b0509a
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2024-02-23 14:59:59 -0700

    added profile link for testing

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,11 @@ router.get('/checkLoginStatus', (req, res) => {
   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
+app.get('/profile', requiresAuth(), (req, res) =>{
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+
 router.use('/recipes', require('./recipes'));
 
 module.exports = router;
