a05c1ab7ff55d409d728bf7d6ccc876cdd04bfcc
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2024-02-23 13:36:25 -0700

    added oauth

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { auth } = require('express-openid-connect');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+router.use(auth(config));
+router.get('/checkLoginStatus', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
 router.use('/recipes', require('./recipes'));
 
