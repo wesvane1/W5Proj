6168c74c136c354dad839f2ef388b45b31837643
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2024-02-23 20:17:41 -0700

    got rid of port, moved it to ENV

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const { auth, requiresAuth } = require('express-openid-connect');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const port = process.env.PORT || 3000;
 const config = {
   authRequired: false,
   auth0Logout: true,
